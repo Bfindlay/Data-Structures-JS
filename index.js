d9d496ab4bbc38c38328d313d591b2a4aafc0a99
bfindlay <bfin5330@uni.sydney.edu.au>
2016-08-22 22:23:19 +1000

    minor text fixes

--- index.js
+++ index.js
@@ -2,15 +2,5 @@
 
 let LinkedList = require('./lib/LinkedList');
 
-let list = new LinkedList();
+let l = new LinkedList();
 
-console.log(list.head);
-list.add(2);
-console.log(list.head);
-list.add(3);
-console.log(list.head);
-list.add(4);
-console.log(list.head);
-list.add(5);
-
-console.log(list.head);
